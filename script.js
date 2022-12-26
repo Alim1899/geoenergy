8d25db38d8d13a4f0d7c9d1c86dd438608ec98ff
Alim Akhchanogli <alimakhchanogli@gmail.com>
2022-12-26 23:06:00 +0400

    Made navbar responsive

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//Adding menu toolbar to navbar
+const toggleBtn = document.querySelector('.toggle');
+const list = document.querySelector('.list');
+
+toggleBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    if(list.classList.contains('toggler'))  list.classList.remove('toggler');
+    else list.classList.add('toggler');
+})
